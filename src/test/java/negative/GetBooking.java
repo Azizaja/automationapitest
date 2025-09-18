b8f7ab33da47bdb4b182f6ffc1ecdd9337132f29
Azizaja <azizsetiawan@students.amikom.ac.id>
2025-09-18 16:09:39 +0700

    add negative test

--- src/test/java/negative/GetBooking.java
+++ src/test/java/negative/GetBooking.java
@@ -1,5 +1,40 @@
 package negative;
 
+import static io.restassured.RestAssured.given;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.response.Response;
+
 public class GetBooking {
+    @Test
+    public void GetBooking_NonExistentID() {
+        System.out.println("Negative Test: Non-Existent Booking ID");
+        int nonExistentId = 999999;
+
+        Response response = given()
+                .baseUri("https://restful-booker.herokuapp.com/booking/" + nonExistentId)
+                .when()
+                .get();
+
+        response.then().log().all();
+        Assert.assertEquals(response.statusCode(), 404, "Should return 404 for non-existent booking");
+        System.out.println("Non-existent booking ID test passed");
+    }
+
+    @Test
+    public void GetBooking_InvalidIDFormat() {
+        System.out.println("Negative Test: Invalid ID Format");
+
+        Response response = given()
+                .baseUri("https://restful-booker.herokuapp.com/booking/invalid_id")
+                .when()
+                .get();
+
+        response.then().log().all();
+        Assert.assertEquals(response.statusCode(), 404, "Should return 404 for invalid ID format");
+        System.out.println("Invalid ID format test passed");
+    }
 
 }
